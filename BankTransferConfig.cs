5569099d5704f94a9568fc7398a3adea5368b11c
Drakkura <gonhgyhjj@student.telkomuniversity.ac.id>
2023-04-12 17:20:57 +0700

    aplikasi bank transfer dengan runtime configuration

---
+++ BankTransferConfig.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace modul8_1302210124
+{
+    internal class BankTransferConfig
+    {
+        public Config config { get; set; }
+        public string path = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.FullName;
+        public string configFileName = "bank_transfer_config.json";
+
+        public BankTransferConfig() {
+            try {
+                ReadConfig();
+            }
+            catch {
+                setDefault();
+                WriteConfig();
+            }
+        }
+        private Config ReadConfig() { 
+            string jsonFromFile = File.ReadAllText(path+'/'+configFileName);
+            config = JsonSerializer.Deserialize<Config>(jsonFromFile);
+            return config;
+        }
+        private void WriteConfig() {
+            JsonSerializerOptions options = new JsonSerializerOptions()
+            {
+                WriteIndented = true
+            };
+
+            String jsonString = JsonSerializer.Serialize(config, options);
+            string fullPath = path + '/' + configFileName;
+            File.WriteAllText(fullPath, jsonString);
+        }
+
+        public void setDefault()
+        {
+            config = new Config("en", new Transfer(25000000, 6500, 15000), 
+                new List<string> { "RTO (real-time)", "SKN", "RTGS", "BI FAST"}, 
+                new Confirm("yes", "ya"));
+        }
+
+        public void ubahBahasa() {
+            config.lang = config.lang == "en" ? "id" : "en";
+        }
+
+
+    }
+
+    public class Confirm { 
+        public string en { get; set; }
+        public string id { get; set; }
+
+        public Confirm() { }
+
+        public Confirm(string en, string id)
+        {
+            this.en = en;
+            this.id = id;
+        }
+    }
+    public class Transfer {
+        public int threshold { get; set; }
+        public int low_fee { get; set; }
+
+        public int high_fee { get; set; }
+
+        public Transfer() { }
+
+        public Transfer(int threshold, int low_fee, int high_fee)
+        {
+            this.threshold = threshold;
+            this.low_fee = low_fee;
+            this.high_fee = high_fee;
+        }
+    }
+
+
+    public class Config {
+        public string lang { get; set; }
+        public Transfer transfer { get; set; }
+        public List<string> methods { get; set; }
+
+        public Confirm confirm { get; set; }
+
+        public Config(){ }
+
+        public Config(string lang, Transfer transfer, List<string> methods, Confirm confirm) { 
+            this.lang = lang;
+            this.transfer = transfer;
+            this.methods = methods;
+            this.confirm = confirm;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,95 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using modul8_1302210124;
+using System.Reflection.Metadata.Ecma335;
+
+internal class program {
+    private static int Main(string[] args)
+    {
+        BankTransferConfig config = new BankTransferConfig();
+
+        if (config.config.lang.Equals("en"))
+        {
+            Console.WriteLine("Please insert the amount of money to transfer:");
+        }
+        else if (config.config.lang.Equals("id"))
+        {
+            Console.WriteLine("Masukkan jumlah uang yang akan di-transfer:");
+        }
+        string tf = Console.ReadLine();
+        int jumTF = (int)Convert.ToInt64(tf); int bTF = 0;
+
+        if (jumTF <= config.config.transfer.threshold)
+        {
+            bTF = config.config.transfer.low_fee;
+        }
+        else if (jumTF >= config.config.transfer.threshold)
+        {
+            bTF = config.config.transfer.high_fee;
+        }
+
+        if (config.config.lang.Equals("en"))
+        {
+            Console.WriteLine("Transfer fee = " + bTF);
+            Console.WriteLine("Total amount = " + (jumTF + bTF));
+        }
+        else if (config.config.lang.Equals("id"))
+        {
+            Console.WriteLine("Biaya Transfer = " + bTF);
+            Console.WriteLine("Total biaya = " + (jumTF + bTF));
+        }
+
+        if (config.config.lang.Equals("en"))
+        {
+            Console.WriteLine("Select transfer method:");
+        }
+        else if (config.config.lang.Equals("id"))
+        {
+            Console.WriteLine("Pilih metode transfer: ");
+        }
+
+        for (int i = 0; i < config.config.methods.Count; i++)
+        {
+            Console.WriteLine((i + 1) + ". " + config.config.methods[i]);
+
+        }
+        Console.Write("Select: ");
+        string metode = Console.ReadLine();
+
+        int metodeTransfer = (int)Convert.ToInt32(tf);
+
+        if (config.config.lang.Equals("en"))
+        {
+            Console.WriteLine("Please typer " + config.config.confirm.en + " to confirm");
+        }
+        else if (config.config.lang.Equals("id"))
+        {
+            Console.WriteLine("Ketik " + config.config.confirm.id + " untuk konfirmasi");
+        }
+
+        string inputKonfirmasi = Console.ReadLine();
+
+        if (config.config.lang.Equals("en"))
+        {
+            if (inputKonfirmasi.Equals(config.config.confirm.en))
+            {
+                Console.WriteLine("The transfer is completed");
+            }
+            else
+            {
+                Console.WriteLine("Transfer is cancelled");
+            }
+        }
+        else if (config.config.lang.Equals("id"))
+        {
+            if (inputKonfirmasi.Equals(config.config.confirm.id))
+            {
+                Console.WriteLine("Proses transfer berhasil");
+            }
+            else
+            {
+                Console.WriteLine("Transfer dibatalkan");
+                Console.WriteLine("Transfer dibatalkan");
+            }
+
+        }
+    }
+}
